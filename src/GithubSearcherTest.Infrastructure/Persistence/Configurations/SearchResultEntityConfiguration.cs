17639764fedf3bcf6fd7be719f919f4cdf598322
dduhovny <dduhovny26@gmail.com>
2022-10-10 14:03:14 +0300

    Добавление id #2

--- src/GithubSearcherTest.Infrastructure/Persistence/Configurations/SearchResultEntityConfiguration.cs
+++ src/GithubSearcherTest.Infrastructure/Persistence/Configurations/SearchResultEntityConfiguration.cs
@@ -8,7 +8,10 @@ namespace GithubSearcherTest.Infrastructure.Persistence.Configurations
     {
         public void Configure(EntityTypeBuilder<SearchResult> builder)
         {
-            builder.HasKey(s => s.Query);
+            builder.HasKey(s => s.Id);
+
+            builder.Property(s => s.Id)
+                .UseIdentityByDefaultColumn();
 
             builder.Property(s => s.Query)
                 .HasMaxLength(300)
@@ -17,6 +20,8 @@ namespace GithubSearcherTest.Infrastructure.Persistence.Configurations
             builder.Property(s => s.Result)
                 .HasColumnType("jsonb")
                 .IsRequired();
+
+            builder.HasIndex(s => s.Query);
         }
     }
 }
